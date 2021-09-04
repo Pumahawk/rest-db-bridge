4beacd1855b3ea1ca0105015f96699d75d3fef3a
Lorenzo Gandino <lorenzoga94@gmail.com>
2021-09-04 17:45:08 +0200

    init

---
+++ src/main/java/com/pumahawk/rest/db/bridge/RestDbBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.pumahawk.rest.db.bridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestDbBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestDbBridgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pumahawk/rest/db/bridge/RestDbBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pumahawk.rest.db.bridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestDbBridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
